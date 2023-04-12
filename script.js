19685f697006feffd176be53b88d79da0f4d95ff
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-12 17:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	for(let index in library){
+		if(library[index].readingStatus){
+			count++;
+		}
+	}
+	return count;
+	
 };
 
 // Do not change the code below
